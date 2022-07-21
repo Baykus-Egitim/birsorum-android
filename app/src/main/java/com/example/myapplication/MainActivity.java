c994e72234bbfd5ebd6ebb6cacd93cfa65b2e896
Hüseyin <iletisim@huseyinyigit.com.tr>
2022-07-21 20:14:26 +0300

    Initial working example

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.loadUrl("http://tutlane.com");
+    }
+}
